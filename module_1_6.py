3403b64b78d73a1c14ba52da4e9acbae08bb0d63
romangatiatullin <mr.artemka02@mail.ru>
2024-09-09 22:43:57 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict = {'Roman': 2004, 'Lera': 2003}
+print(my_dict)
+print(my_dict['Roman'])
+my_dict['Pavel'] = 2000
+print(my_dict['Pavel'])
+my_dict.update({'Denis': 2001, 'Potap': 2005})
+print(my_dict)
+a = my_dict.pop('Potap')
+print(a)
+print(my_dict)
+my_set = set_ = {1,2,'apple',2,False,'apple','orange',1,False}
+print(my_set)
+set_.add(123)
+set_.add('coconut')
+set_.discard(False)
+print(my_set)
