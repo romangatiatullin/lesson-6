2bd1c340d4bd6893c7574f1fde5e60ce4aab3a1e
romangatiatullin <mr.artemka02@mail.ru>
2024-10-06 23:04:48 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,8 +1,7 @@
 my_dict = {'Roman': 2004, 'Lera': 2003}
 print(my_dict)
 print(my_dict['Roman'])
-my_dict['Pavel'] = 2000
-print(my_dict['Pavel'])
+print(my_dict.get('Pavel'))
 my_dict.update({'Denis': 2001, 'Potap': 2005})
 print(my_dict)
 a = my_dict.pop('Potap')
